1d74a1cd59372192d3f1f25440d424cd8103ad59
ziad <ziadtaha567@gmail.com>
2024-07-19 10:14:12 +0300

    finished splash screen.

---
+++ app/src/main/java/com/example/islamiapp/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.islamiapp.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.islamiapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        }
+
+}
---
+++ app/src/main/java/com/example/islamiapp/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.islamiapp.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.islamiapp.R
+import com.example.islamiapp.home.HomeActivity
+
+class SplashActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startHomeScreen()
+    }
+
+    private fun startHomeScreen() {
+
+        Handler().postDelayed({
+            val intent = Intent(this , HomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+
+
+    }
+}
